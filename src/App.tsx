a2225e803b1fd131512594b30890da7f810010a1
Lyn <0xlynett@proton.me>
2024-06-27 03:28:34 +0000

    change text to not include fc/solana

--- src/App.tsx
+++ src/App.tsx
@@ -162,8 +162,10 @@ export default function App() {
 
             <ul className="text-lg text-transparent bg-gradient-to-b from-foreground to-foreground/40 bg-clip-text">
               <li>Being perceived as old and useless technology.</li>
-              <li>Elite builders hidden away on Farcaster.</li>
-              <li>The narrative being controlled by Solana.</li>
+              <li>Elite builders not getting enough attention.</li>
+              <li>
+                The narrative being controlled by anti-Ethereum psyops experts.
+              </li>
               <li>Alt-L1s spreading FUD about Ethereum daily.</li>
               <li>Competing innovations stealing the spotlight.</li>
             </ul>
