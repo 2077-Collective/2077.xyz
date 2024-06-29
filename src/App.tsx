d24539abcb04d375959e34f833749818934de472
Lyn <0xlynett@proton.me>
2024-06-29 15:48:27 +0000

    set max size xl

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
   const { theme } = useTheme();
 
   return (
-    <>
+    <div className="max-w-screen-xl">
       <header className="p-8 relative w-full pb-80 max-sm:h-screen">
         <h1 className="text-3xl">We are the 2077 Collective.</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-b from-foreground to-foreground/60 text-xl">
@@ -348,6 +348,6 @@ export default function App() {
           BILLION USERS|LET'S ONBOARD THE NEXT BILLION USERS|
         </Marquee>
       </footer>
-    </>
+    </div>
   );
 }
